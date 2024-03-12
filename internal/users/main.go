2c10776834bf83d6c6878ba370702b6da9620ce2
QMStar <qmstar0@qq.com>
2024-03-12 21:30:54 +0800

    优化事件溯源模块

--- internal/users/main.go
+++ internal/users/main.go
@@ -1,1 +1,31 @@
 package users
+
+import (
+	"blog/pkg/env"
+	"blog/pkg/mongodb"
+	"blog/pkg/rediscache"
+	"blog/pkg/shutdown"
+	"common/server"
+	"context"
+	"github.com/go-chi/chi/v5"
+	"users/adapters"
+	"users/ports"
+)
+
+func init() {
+	env.Load()
+
+	dbCloseFn := mongodb.Init()
+	shutdown.OnShutdown(func() error { return dbCloseFn(context.Background()) })
+
+	cacheCloseFn := rediscache.Init()
+	shutdown.OnShutdown(cacheCloseFn)
+}
+func main() {
+	app := adapters.NewApp()
+	server.RunHttpServer(":3000", func(r chi.Router) {
+		ports.HandlerWithOptions(ports.NewHttpServer(app), ports.ChiServerOptions{
+			BaseRouter: r,
+		})
+	})
+}
